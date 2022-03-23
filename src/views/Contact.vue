a764070f6e1fc06ff0ca0f449849c51d4319c703
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2022-03-23 21:08:42 +0000

    trying axios for form submissions

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,39 +1,58 @@
 <template>
   <h1>Contact Us</h1>
   <form
+    class="contact-form"
     name="contact"
     method="POST"
     data-netlify="true"
     action="/"
     data-netlify-honeypot="bot-field"
+    @submit.prevent="handleSubmit"
   >
     <input type="hidden" name="form-name" value="contact" />
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label
-        >Your Role:
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select></label
-      >
-    </p>
-    <p>
-      <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
+    <label for="">Name:</label>
+    <input v-model="name" type="text" />
+    <label for="">Email:</label>
+    <input v-model="email" type="email" name="" id="" />
+    <label for="">Your message:</label>
+    <textarea v-model="message" name="" id="" cols="30" rows="10"></textarea>
+    <button>Submit</button>
   </form>
+  <!-- <p>{{ name }}</p>
+  <p>{{ email }}</p>
+  <p>{{ message }}</p> -->
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import { ref } from "@vue/reactivity";
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    const name = ref("");
+    const email = ref("");
+    const message = ref("");
+    const router = useRouter();
+    const handleSubmit = () => {
+      axios
+        .post("/", {
+          name: name.value,
+          email: email.value,
+          message: message.value,
+        })
+        .then(() => {
+          router.push("/FormSuccess");
+        });
+    };
+    return { name, email, message, handleSubmit };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.contact-form {
+  /* border: 1px solid red; */
+  display: flex;
+  flex-direction: column;
+}
+</style>
