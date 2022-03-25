275d924609a19344eef9559debc0dce50482f586
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2022-03-25 01:42:30 +0000

    contact ramesss

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Contact Akhilesh</h1>
+  <h1>Contact Ramessss</h1>
   <form
     class="contact-form"
     name="contact"
@@ -7,67 +7,61 @@
     action="/"
     data-netlify="true"
     data-netlify-honeypot="bot-field"
-    @submit.prevent="handleSubmit"
   >
     <input type="hidden" name="form-name" value="contact" />
     <label for="">Name:</label>
-    <input v-model="form.name" name="name" type="text" />
+    <input name="name" type="text" />
     <label for="">Email:</label>
-    <input v-model="form.email" type="email" name="email" id="" />
+    <input type="email" name="email" id="" />
     <label for="">Your message:</label>
-    <textarea
-      v-model="form.message"
-      name="message"
-      id=""
-      cols="30"
-      rows="10"
-    ></textarea>
+    <textarea name="message" id="" cols="30" rows="10"></textarea>
     <button>Submit</button>
   </form>
-  <!-- <p>{{ name }}</p>
-  <p>{{ email }}</p>
-  <p>{{ message }}</p> -->
 </template>
 
+//
 <script>
-import axios from "axios";
-export default {
-  data: () => ({
-    form: {
-      name: "",
-      email: "",
-      message: "",
-    },
-  }),
-  methods: {
-    encode(data) {
-      return Object.keys(data)
-        .map(
-          (key) =>
-            `${encodeURIComponent(key)} = ${encodeURIComponent(data[key])}`
-        )
-        .join("&");
-    },
-    handleSubmit() {
-      const axiosConfig = {
-        header: { "Content-Type": "application/x-www-form-urlencoded" },
-      };
-      axios
-        .post(
-          "/",
-          this.encode({
-            "form-name": "contact",
-            ...this.form,
-          }),
-          axiosConfig
-        )
-        .then(() => {
-          console.log("successfully sent");
-        })
-        .catch((error) => console.log(error));
-    },
-  },
-};
+// import axios from "axios";
+// export default {
+//   data() {
+//     return {
+//       form: {
+//         name: "",
+//         email: "",
+//         message: "",
+//       },
+//     };
+//   },
+//   methods: {
+//     encode(data) {
+//       return Object.keys(data)
+//         .map(
+//           (key) =>
+//             `${encodeURIComponent(key)} = ${encodeURIComponent(data[key])}`
+//         )
+//         .join("&");
+//     },
+//     handleSubmit() {
+//       const axiosConfig = {
+//         header: { "Content-Type": "application/x-www-form-urlencoded" },
+//       };
+//       axios
+//         .post(
+//           "/",
+//           this.encode({
+//             "form-name": "contact",
+//             ...this.form,
+//           }),
+//           axiosConfig
+//         )
+//         .then(() => {
+//           console.log("successfully sent");
+//         })
+//         .catch((error) => console.log(error));
+//     },
+//   },
+// };
+//
 </script>
 
 <style>
